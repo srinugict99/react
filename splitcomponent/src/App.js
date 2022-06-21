9e4c48d7992a5c0ec9389fd3d68a3272f458d547
B Srinivasarao <B.Srinivasarao@AD.JKTECH.COM>
2022-06-21 17:59:19 +0530

    split component

--- splitcomponent/src/App.js
+++ splitcomponent/src/App.js
@@ -17,7 +17,7 @@ function App() {
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
-      amount: 452,
+      amount: 451,
       date: new Date(2021, 5, 12),
     },
   ];
--- splitcomponent/src/components/ExpenseDate.js
+++ splitcomponent/src/components/ExpenseDate.js
@@ -1,13 +1,14 @@
+import './ExpenseDate.css'
 const ExpenseDate= (props) =>{
     console.log('props', props)
     const day= props.date.toLocaleString('en-US', {month: '2-digit'});
     const month= props.date.toLocaleString('en-US', {month: 'long'});
     const year= props.date.getFullYear();
     return (
-        <div>
-            <div>{day}</div>
-            <div>{month}</div>
-            <div>{year}</div>
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__year'>{year}</div>
+            <div className='expense-date__day'>{day}</div>
         </div>
     )
     
