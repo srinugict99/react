e33861a6320a446aec84983de38f12126fe5f297
B Srinivasarao <B.Srinivasarao@AD.JKTECH.COM>
2022-06-21 12:26:25 +0530

    customcomponents

---
+++ custom-component/src/App.js
@@ -0,0 +1,10 @@
+import ExpenseItem from './components/ExpenseItem'
+function App() {
+  return (
+    <div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-component/src/components/ExpenseItem.js
@@ -0,0 +1,4 @@
+function ExpenseItem(){
+    return <h2>custom components</h2>
+}
+export default ExpenseItem;
